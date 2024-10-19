b08babef45ef3835e02ce319c7905c52616f7cb4
Tamizhanban212 <tamizhanbanag@gmail.com>
2024-10-19 12:14:02 +0530

    commited

---
+++ owl/owl_robot_sdk_examples/traj_circle.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+try:
+    from math import pi, tau, dist, fabs, cos
+except:  # For Python 2 compatibility
+    from math import pi, fabs, cos, sqrt
+
+    tau = 2.0 * pi
+
+    def dist(p, q):
+        return sqrt(sum((p_i - q_i) ** 2.0 for p_i, q_i in zip(p, q)))
+
+
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+
+class MoveGroupPythonInterfaceTutorial(object):
+    """MoveGroupPythonInterfaceTutorial"""
+
+    def _init_(self):
+        super(MoveGroupPythonInterfaceTutorial, self)._init_()
+
+        ## BEGIN_SUB_TUTORIAL setup
+        ##
+        ## First initialize `moveit_commander`_ and a `rospy`_ node:
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node("move_group_python_interface_tutorial", anonymous=True)
+
+        ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
+        ## kinematic model and the robot's current joint states
+        robot = moveit_commander.RobotCommander()
+
+        ## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
+        ## for getting, setting, and updating the robot's internal understanding of the
+        ## surrounding world:
+        scene = moveit_commander.PlanningSceneInterface()
+
+        ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+        ## to a planning group (group of joints).  In this tutorial the group is the primary
+        ## arm joints in the Panda robot, so we set the group's name to "panda_arm".
+        ## If you are using a different robot, change this value to the name of your robot
+        ## arm planning group.
+        ## This interface can be used to plan and execute motions:
+        group_name = "arm"
+        move_group = moveit_commander.MoveGroupCommander(group_name)
+
+        ## Create a `DisplayTrajectory`_ ROS publisher which is used to display
+        ## trajectories in Rviz:
+        display_trajectory_publisher = rospy.Publisher(
+            "/move_group/display_planned_path",
+            moveit_msgs.msg.DisplayTrajectory,
+            queue_size=20,
+        )
+
+        ## END_SUB_TUTORIAL
+
+        ## BEGIN_SUB_TUTORIAL basic_info
+        ##
+        ## Getting Basic Information
+        ## ^^^^^^^^^^^^^^^^^^^^^^^^^
+        # We can get the name of the reference frame for this robot:
+        planning_frame = move_group.get_planning_frame()
+        print("============ Planning frame: %s" % planning_frame)
+
+        # We can also print the name of the end-effector link for this group:
+        eef_link = move_group.get_end_effector_link()
+        print("============ End effector link: %s" % eef_link)
+
+        # We can get a list of all the groups in the robot:
+        group_names = robot.get_group_names()
+        print("============ Available Planning Groups:", robot.get_group_names())
+
+        # Sometimes for debugging it is useful to print the entire state of the
+        # robot:
+        print("============ Printing robot state")
+        print(robot.get_current_state())
+        print("")
+        ## END_SUB_TUTORIAL
+
+    #     self.box_name = ""
+    #     self.robot = robot
+    #     self.scene = scene
+    #     self.move_group = move_group
+    #     self.display_trajectory_publisher = display_trajectory_publisher
+    #     self.planning_frame = planning_frame
+    #     self.eef_link = eef_link
+    #     self.group_names = group_names
+
+    # def go_to_pose_goal(self):
+    #     # Copy class variables to local variables to make the web tutorials more clear.
+    #     # In practice, you should use the class variables directly unless you have a good
+    #     # reason not to.
+    #     move_group = self.move_group
+
+    #     ## BEGIN_SUB_TUTORIAL plan_to_pose
+    #     ##
+    #     ## Planning to a Pose Goal
+    #     ## ^^^^^^^^^^^^^^^^^^^^^^^
+    #     ## We can plan a motion for this group to a desired pose for the
+    #     ## end-effector:
+    #     pose_goal = geometry_msgs.msg.Pose()
+    #     pose_goal.orientation.w = 1.0
+    #     pose_goal.position.x = 0.4
+    #     pose_goal.position.y = 0.1
+    #     pose_goal.position.z = 0.4
+
+    #     move_group.set_pose_target(pose_goal)
+
+    #     ## Now, we call the planner to compute the plan and execute it.
+    #     # go() returns a boolean indicating whether the planning and execution was successful.
+    #     success = move_group.go(wait=True)
+    #     # Calling stop() ensures that there is no residual movement
+    #     move_group.stop()
+    #     # It is always good to clear your targets after planning with poses.
+    #     # Note: there is no equivalent function for clear_joint_value_targets().
+    #     move_group.clear_pose_targets()
+
+    # def plan_cartesian_path(self, scale=1):
+    #     # Copy class variables to local variables to make the web tutorials more clear.
+    #     # In practice, you should use the class variables directly unless you have a good
+    #     # reason not to.
+    #     move_group = self.move_group
+
+    #     ## BEGIN_SUB_TUTORIAL plan_cartesian_path
+    #     ##
+    #     ## Cartesian Paths
+    #     ## ^^^^^^^^^^^^^^^
+    #     ## You can plan a Cartesian path directly by specifying a list of waypoints
+    #     ## for the end-effector to go through. If executing  interactively in a
+    #     ## Python shell, set scale = 1.0.
+    #     ##
+    #     waypoints = []
+
+    #     wpose = move_group.get_current_pose().pose
+    #     wpose.position.z -= scale * 0.1  # First move up (z)
+    #     wpose.position.y += scale * 0.2  # and sideways (y)
+    #     waypoints.append(copy.deepcopy(wpose))
+
+    #     wpose.position.x += scale * 0.1  # Second move forward/backwards in (x)
+    #     waypoints.append(copy.deepcopy(wpose))
+
+    #     wpose.position.y -= scale * 0.1  # Third move sideways (y)
+    #     waypoints.append(copy.deepcopy(wpose))
+
+    #     # We want the Cartesian path to be interpolated at a resolution of 1 cm
+    #     # which is why we will specify 0.01 as the eef_step in Cartesian
+    #     # translation.  We will disable the jump threshold by setting it to 0.0,
+    #     # ignoring the check for infeasible jumps in joint space, which is sufficient
+    #     # for this tutorial.
+    #     # Convert waypoints to boost python list (moveit needs boost::python list)
+    #     waypoints_boost_list = rospy.to_list(waypoints)
+
+    # # Call compute_cartesian_path with correct types
+    #     (plan, fraction) = move_group.compute_cartesian_path(
+    #     waypoints_boost_list, 0.01, 0.0  # waypoints to follow  # eef_step
+    # )  # jump_threshold
+
+    #     ## END_SUB_TUTORIAL
+
+    # def execute_plan(self, plan):
+    #     # Copy class variables to local variables to make the web tutorials more clear.
+    #     # In practice, you should use the class variables directly unless you have a good
+    #     # reason not to.
+    #     move_group = self.move_group
+
+    #     ## BEGIN_SUB_TUTORIAL execute_plan
+    #     ##
+    #     ## Executing a Plan
+    #     ## ^^^^^^^^^^^^^^^^
+    #     ## Use execute if you would like the robot to follow
+    #     ## the plan that has already been computed:
+    #     move_group.execute(plan, wait=True)
+
+    #     ## *Note:* The robot's current joint state must be within some tolerance of the
+    #     ## first waypoint in the `RobotTrajectory`_ or `execute()` will fail
+    #     ## END_SUB_TUTORIAL
+
+
+def main():
+    try:
+        
+        tutorial = MoveGroupPythonInterfaceTutorial()
+        
+        # tutorial.go_to_pose_goal()
+        # cartesian_plan, fraction = tutorial.plan_cartesian_path()
+        # tutorial.execute_plan(cartesian_plan)
+
+        
+        print("============ Python tutorial demo complete!")
+    except rospy.ROSInterruptException:
+        return
+    except KeyboardInterrupt:
+        return
+
+
+if __name__ == "__main__":
+    main()
